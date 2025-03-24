b095c99403eeaba36cd31354a25f0b93404bf9b5
Ishika Sharma <ishikasharma7224@gmail.com>
2025-03-24 10:35:42 +0530

    Add files via upload

---
+++ factorial.java
@@ -0,0 +1,14 @@
+
+public class factorial {
+	public static void main(String[] args)
+	{
+		int num=4;
+		int fact=1;
+		for(int i=1;i<=num;i++)
+		{
+			fact=fact*i;
+		}
+		System.out.println(fact);
+	}
+
+}
